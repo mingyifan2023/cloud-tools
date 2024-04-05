d86b7112d0c7b1a7062cd49dad9bf35b786207a2
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-04-05 16:18:30 +0900

    Create t.sh

---
+++ 4. 清空服务器所有log的内容/t.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 找到所有大于 1KB 的日志文件并清空
+echo "清空超过 1KB 的日志文件："
+echo "=================="
+
+# 查找大于 5KB 的日志文件，并保存到变量 logs 中
+logs=$(find / -type f -name "*.log" -size +1k)
+
+# 遍历 logs 中的每个日志文件，并使用 cat /dev/null 来清空它们
+for log in $logs; do
+    echo "clean logs: $log"
+    cat /dev/null > "$log"
+done
